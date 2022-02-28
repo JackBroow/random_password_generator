7d1f094fe679844e37667a37e1ab6b6cded7e402
Jack Brow <77904282+JackBrow-Dev@users.noreply.github.com>
2022-02-28 19:21:42 +0100

    Add files via upload

---
+++ password_generator/main.py
@@ -0,0 +1,19 @@
+# Random password generator
+# Creator: JackBrow-Dev
+
+import random
+
+# No need to change this section
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+NUMBER = "0123456789"
+Symbol = "[]{}#()*;+_-"
+
+# If you don't want any of the features in your password you can't delete it in the line below
+together = lower + upper + NUMBER + Symbol
+# Set the length to whatever you want (default: 9).
+length = 9
+# DO NOT TOUCH!
+password = "".join(random.sample(together,length))
+# Irrelevant code, you can change it if you want.
+print("Generated password: ",password)
